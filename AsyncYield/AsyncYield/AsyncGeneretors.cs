7e78c7828bc4e7b60b475f97b4a2846aac492fb1
Prasadnair <prasadnair44@gmail.com>
2023-08-12 23:00:16 -0400

    Combine Yield and Async

---
+++ AsyncYield/AsyncYield/AsyncGeneretors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncYield
+{
+    internal class AsyncGeneretors
+    {
+
+        public static async IAsyncEnumerable<int> GenerateNumbersAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(100); // Simulate asynchronous work
+                yield return i;
+            }
+        }
+
+    }
+}
---
+++ AsyncYield/AsyncYield/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsyncYield;
+
+Console.WriteLine("Combine Async and Yield");
+
+var numbers = AsyncGeneretors.GenerateNumbersAsync();
+
+await foreach (var number in numbers )
+{
+    Console.WriteLine(number);
+}
+Console.Read();
